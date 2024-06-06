a6611cacfb687ec082152025486bf7d6a58aae5c
Ng-Harry <ngereharry5@gmail.com>
2024-06-06 01:33:08 +0100

    host

--- ansaa_server/settings.py
+++ ansaa_server/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insegergrfdgvq w 4r35y6745rab1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','.now.sh','www.dotsassets.com','dotsassets.com']
+ALLOWED_HOSTS = ['127.0.0.1','.onrender.com','*']
 
 
 # Application definition
