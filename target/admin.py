6d822fe1c4329f48c8529c661fa63a385568b06f
Ng-Harry <ngereharry5@gmail.com>
2024-06-21 18:22:35 +0100

    delete target app

--- target/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import MonthlyTarget
-
-
-
-admin.site.register(MonthlyTarget)
--- target/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('user-billboard-stats/monthly-stats', views.MonthlyTarget.as_view(), name='monthly-stats'),
-]
