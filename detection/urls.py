389f0a6b53a35ba580e8831c543aa792c334d7f9
Ng-Harry <ngereharry5@gmail.com>
2024-07-07 20:19:36 +0100

    remove detection app

--- detection/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('detect-and-predict-billboard/', views.process_images, name='detect_and_predict_billboard'),
-    path('validate-reference-image/', views.detect_book_in_image, name='validate_reference_image'),
-]
