2a7e75234a64ef8bc492d37b5363a46eae684e2f
Ng-Harry <ngereharry5@gmail.com>
2024-07-17 14:55:07 +0100

    update

--- ansaa_server/urls.py
+++ ansaa_server/urls.py
@@ -17,8 +17,56 @@ urlpatterns = [
     path('report/', include('report.urls')),
     path('media-asset/', include("media_asset.urls")),
     path('targets/', include("ansa_target.urls")),
-    path('detection/', include("detection.urls")),
+    # path('detection/', include("detection.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+# server {
+#     server_name ansaa.duckdns.org;
+
+#     location = /favicon.ico { access_log off; log_not_found off; }
+
+#     location /static/ {
+#         root /root/ansaa-bknd; 
+#     }
+
+#     location /media/ {
+#         root /root/ansaa-bknd;
+#     }
+
+#     location / {
+#         include proxy_params;
+#         proxy_pass http://unix:/root/ansaa-bknd/gunicorn.sock;
+#         proxy_set_header X-Real-IP $remote_addr;
+#         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#         proxy_set_header X-Forwarded-Proto $scheme;
+#     }
+
+#     error_page 500 502 503 504 /50x.html;
+#     location = /50x.html {
+#         root /usr/share/nginx/html;
+#     }
+
+#     listen 443 ssl; # managed by Certbot
+#     ssl_certificate /etc/letsencrypt/live/ansaa.duckdns.org/fullchain.pem; # managed by Certbot
+#     ssl_certificate_key /etc/letsencrypt/live/ansaa.duckdns.org/privkey.pem; # managed by Certbot
+#     include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+#     ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+
+# }
+# server {
+#     if ($host = ansaa.duckdns.org) {
+#         return 301 https://$host$request_uri;
+#     } # managed by Certbot
+
+
+#     listen 80;
+#     server_name ansaa.duckdns.org;
+#     return 404; # managed by Certbot
+
+
+# }
