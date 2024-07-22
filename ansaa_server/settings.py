ce0de8a2629837f1edc7373a961afa680b149ea3
Ng-Harry <ngereharry5@gmail.com>
2024-07-22 19:44:16 +0100

    added ip

--- ansaa_server/settings.py
+++ ansaa_server/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insegergrfdgvq w 4r35y6745rab1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','www.dotsassets.com','dotsassets.com','*','https://ansaa.duckdns.org']
+ALLOWED_HOSTS = ['67.205.190.161','www.dotsassets.com','dotsassets.com','*','https://ansaa.duckdns.org']
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
