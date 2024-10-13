ededb40109fd8e4543c2cd55afae558d0ea3476b
Ng-Harry <ngereharry5@gmail.com>
2024-10-13 14:28:22 +0100

    add password reset and registration password mail config

--- authentication/task.py
+++ authentication/task.py
@@ -1,13 +1,11 @@
 import threading, random, string
 from django.utils import timezone
 from django.conf import settings
-from . models import OTP
 from django.core.mail import EmailMultiAlternatives
 from django.core.mail import send_mail
 from django.template import loader
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
-
 
 
 class EmailThread(threading.Thread):
@@ -21,8 +19,56 @@ class EmailThread(threading.Thread):
         self.email_message.send()
 
 
+# def generate_otp():
+#         # Generate a 4-digit OTP
+#         otp = ''.join(random.choices(string.digits, k=4))
+#         return otp
+
+# def send_otp_email(email, otp_code):
+#     # Send OTP email
+#     email_subject = '[Ansaa OTP] Verification Code'
+#     template = loader.get_template('mail_template.txt')
+#     parameters = {'otp': otp_code}
+#     email_content = template.render(parameters)
+
+#     email_message = EmailMultiAlternatives(
+#         email_subject,
+#         email_content,
+#         settings.EMAIL_HOST_USER,
+#         [email]
+#     )
+#     email_message.content_subtype = 'html'
+#     EmailThread(email_message).start()
 
 
-    
+def registration_notice(email, password, fullname):
+    # Send account creation mail
+    email_subject = 'You have a new Ansa account'
+    template = loader.get_template('registration.txt')
+    parameters = {'password':password, "name": fullname}
+    email_content = template.render(parameters)
 
+    email_message = EmailMultiAlternatives(
+        email_subject,
+        email_content,
+        settings.EMAIL_HOST_USER,
+        [email]
+    )
+    email_message.content_subtype = 'html'
+    EmailThread(email_message).start()
 
+def password_reset(email, password, fullname):
+    # Send password reset mail
+    email_subject = 'Password reset notification'
+    template = loader.get_template('change_password.txt')
+    parameters = {'password':password, "name": fullname}
+    email_content = template.render(parameters)
+
+    email_message = EmailMultiAlternatives(
+        email_subject,
+        email_content,
+        settings.EMAIL_HOST_USER,
+        [email]
+    )
+    email_message.content_subtype = 'html'
+    EmailThread(email_message).start()
