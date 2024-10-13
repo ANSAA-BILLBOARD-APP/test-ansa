12292d239e6954de476c991fbc9d50b41e9f0910
Ng-Harry <ngereharry5@gmail.com>
2024-10-13 14:33:18 +0100

    refactor login and profile apiview

--- authentication/views.py
+++ authentication/views.py
@@ -12,11 +12,9 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.generics import RetrieveUpdateAPIView, RetrieveAPIView
 from rest_framework import status
-from . serializers import RequestOTPSerializer, LogoutSerializer, OTPVerificationSerializer, ProfileSerializer, LoginSerializer
+from . serializers import LogoutSerializer, ProfileSerializer, LoginSerializer
 from . models import AnsaaUser, OTP
 from todo.models import Task
-from . task import generate_otp, EmailThread, send_otp_email, send_otp_sms
-import asyncio
 from drf_spectacular.utils import extend_schema
 
 
@@ -41,6 +39,7 @@ class LogoutView(APIView):
                 return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({'error': 'Refresh token not provided'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class UserProfileViews(APIView):
@@ -81,41 +80,42 @@ class UserProfileViews(APIView):
         serializer = ProfileSerializer(user_profile)
         return Response(serializer.data)
 
-@extend_schema(
-    description='The users "phone number" or "email" is required for authentication on this login endpoint.',
-    summary='User Login'
-)
+
+
 class LoginAPIView(APIView):
     serializer_class = LoginSerializer
+
     def post(self, request, format=None):
-        serializer = LoginSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
+        
         if serializer.is_valid():
             email = serializer.validated_data.get("email")
-            phone_number = serializer.validated_data.get("phone_number")
-            
+            password = serializer.validated_data.get("password")
 
-            #if otp is in record and it's verified
-            existing_otp = OTP.objects.filter(
-            Q(email=email) | Q(phone_number=phone_number)
-            ).first()
-            if existing_otp:
-                email = existing_otp.email
-                if email:
-                    otp_record = OTP.objects.get(email=email, verified=True)
-                    if not otp_record.is_expired():
-                        user = AnsaaUser.objects.filter(email=email).first()
-                        authenticated_user = authenticate(request, email=email)
-                        if authenticated_user:
-                            refresh = RefreshToken.for_user(authenticated_user)
-                            otp_record.delete()
+            user = AnsaaUser.objects.filter(email=email).first()
+            if not user:
+                return Response(
+                    {'error': 'User with this email does not exist'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
 
-                            return Response({'refresh': str(refresh), 'access': str(refresh.access_token),}, status=status.HTTP_200_OK)  
-                        else:
-                            return Response({'error': 'Invalid user credentials'}, status.status.HTTP_400_BAD_REQUEST)
-                    else:
-                        otp_record.delete()
-                        return Response({'error':'OTP expired, request for another'}, status=status.HTTP_400_BAD_REQUEST)
-                
-            return Response({'error': 'Invalid user credentials'}, status=status.HTTP_400_BAD_REQUEST)  
+            authenticated_user = authenticate(request, email=email, password=password)
+
+            if authenticated_user:
+                refresh = RefreshToken.for_user(authenticated_user)
+
+                return Response(
+                    {
+                        'refresh': str(refresh),
+                        'access': str(refresh.access_token),
+                    },
+                    status=status.HTTP_200_OK
+                )
+            else:
+                return Response(
+                    {'error': 'Invalid email or password'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        # Return serializer errors if validation fails
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
