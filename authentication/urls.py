52bd98cae926c7e641fd3b30e72ba182238ccd4b
Ng-Harry <ngereharry5@gmail.com>
2024-10-13 14:29:52 +0100

    update/change auth api urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,13 +2,19 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from .admin import AnsaaUserAdmin
+from django.contrib import admin
+from .models import AnsaaUser
+
+
+admin_site = admin.site
+ansaa_user_admin = AnsaaUserAdmin(model=AnsaaUser, admin_site=admin_site)
 
 urlpatterns = [
-    path('api/otp-request/', views.RequestOTP.as_view()),
-    path('api/logout/', views.LogoutView.as_view()),
-    path('api/validate-otp/', views.ValidateOTPView.as_view()),
-    path('api/profile/', views.UserProfileViews.as_view()),
-    path('api/login/', views.LoginAPIView.as_view()),
-    path("api/api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path('auth/logout/', views.LogoutView.as_view()),
+    path('auth/profile/', views.UserProfileViews.as_view()),
+    path('auth/login/', views.LoginAPIView.as_view()),
+    path("auth/api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("", SpectacularSwaggerView.as_view(url_name="schema")),
+    path('authentication/', include(ansaa_user_admin.get_urls())),
 ]
